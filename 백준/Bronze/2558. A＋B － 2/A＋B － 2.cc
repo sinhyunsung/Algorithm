83a66d00e29724fa58e033e011d6a6aec8c7ddd8
sinhyunsung <37778339+sinhyunsung@users.noreply.github.com>
2022-09-01 12:52:38 +0000

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+	
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+
+}
