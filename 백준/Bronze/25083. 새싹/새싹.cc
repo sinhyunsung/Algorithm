f3bfdb9abb777bb2c45fffda05469a77224c2839
sinhyunsung <37778339+sinhyunsung@users.noreply.github.com>
2023-02-08 12:44:29 +0000

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string msg = R"(         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |)";
+    
+    std::cout << msg;
+
+}
