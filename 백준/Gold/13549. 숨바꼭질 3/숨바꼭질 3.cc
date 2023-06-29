ad2d2276a4a995a23d38cbd898689460ecc73cf6
sinhyunsung <37778339+sinhyunsung@users.noreply.github.com>
2023-06-30 01:50:19 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 4 ms, Memory: 3196 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,60 +1,61 @@
+#include <vector>
 #include <iostream>
+#include <deque>
 #include <algorithm>
 #include <set>
-#include <map>
+#include <stack>
+#include <bitset>
+#include <queue>
+#include <unordered_map>
 #include <cmath>
 #include <string>
-#include <deque>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <math.h>
-
-#define ll long long int
 
 using namespace std;
 
+#define ll long long
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    
-    int a, b;
+int n, k;
 
-    deque<pair<int, int>> bfs;
-
-    int visit[100001] = {0};
-
-    cin >> a >> b;
-
-    int now, time;
-
-    bfs.push_back(make_pair(a, 0));
-    while (!bfs.empty()) {
-        now = bfs.front().first;
-        time= bfs.front().second;
-        bfs.pop_front();
-        if (now == b) {
-            cout << time;
-            break;
-        }
-        if (now * 2 < 100001 && visit[now * 2] == 0) {
-            bfs.push_front(make_pair(now * 2, time));
-            visit[now * 2] = 1;
-        }
-        if (now + 1 < 100001 && visit[now + 1] == 0) {
-            bfs.push_back(make_pair(now + 1, time + 1));
-            visit[now + 1] = 1;
-        }
-        if (now - 1 >= 0 && visit[now - 1] == 0) {
-            bfs.push_back(make_pair(now - 1, time + 1));
-            visit[now - 1] = 1;
-        }
+int dp[100001];
+int visit[100001] = { 0 };
 
 
-    }
 
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	cin >> n >> k;
 
+	fill_n(dp, 100001, 100001);
+
+	deque<pair<int, int>> q;
+
+	q.push_back(make_pair(n, 0));
+	while (!q.empty()) {
+		
+		int x = q.front().first;
+		int t = q.front().second;
+		q.pop_front();
+
+		if (visit[x]) {
+			continue;
+		}
+
+		dp[x] = t;
+		visit[x] = 1;
+		
+		if (x * 2 < 100001 && !visit[x * 2]) {
+			q.push_front(make_pair(2 * x, t));
+		}
+		if (x + 1 < 100001 && !visit[x + 1]) {
+			q.push_back(make_pair(x + 1, t + 1));
+		}
+		if (x - 1 >= 0 && !visit[x - 1]) {
+			q.push_back(make_pair(x - 1, t + 1));
+		}
+	}
+
+	cout << dp[k];
 }
