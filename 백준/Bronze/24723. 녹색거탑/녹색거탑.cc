aff674cc3546993914c94660c046f2d168a78da2
sinhyunsung <37778339+sinhyunsung@users.noreply.github.com>
2024-03-31 00:56:05 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin>>n;
+
+    cout<<pow(2,n);
+
+    return 0;
+}
